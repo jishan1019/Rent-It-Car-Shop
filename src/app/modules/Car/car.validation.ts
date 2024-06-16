f68b1d7ba3bd26ae83a00d342b44f9f56d337eba
jishan <imranislamjishan80@gmail.com>
2024-06-16 21:41:09 +0600

    card validation schema create

--- src/app/modules/Car/car.validation.ts
+++ src/app/modules/Car/car.validation.ts
@@ -1,27 +1,28 @@
 import { z } from "zod";
 
-export const userValidationSchema = z.object({
+export const carValidationSchema = z.object({
   body: z.object({
     name: z.string({ required_error: "Name is required." }),
-    email: z.string({ required_error: "Email is required." }).email(),
-    password: z.string({ required_error: "password is required." }),
-    phone: z.string({ required_error: "phone is required." }),
-    address: z.string({ required_error: "address is required." }),
+    description: z.string({ required_error: "Description is required." }),
+    color: z.string({ required_error: "Color is required." }),
+    isElectric: z.boolean({ required_error: "isElectric is required." }),
+    features: z.array(z.string(), { required_error: "Features are required." }),
+    pricePerHour: z.number({ required_error: "Price per hour is required." }),
   }),
 });
 
-export const updateUserValidationSchema = z.object({
+export const updateCarValidationSchema = z.object({
   body: z.object({
     name: z.string().optional(),
-    email: z.string().optional(),
-    role: z.enum([...Role] as [string, ...string[]]).optional(),
-    password: z.string().optional(),
-    phone: z.string().optional(),
-    address: z.string().optional(),
+    description: z.string().optional(),
+    color: z.string().optional(),
+    isElectric: z.boolean().optional(),
+    features: z.array(z.string()).optional(),
+    pricePerHour: z.number().optional(),
   }),
 });
 
-export const UserValidationSchema = {
-  userValidationSchema,
-  updateUserValidationSchema,
+export const CarValidationSchema = {
+  carValidationSchema,
+  updateCarValidationSchema,
 };
